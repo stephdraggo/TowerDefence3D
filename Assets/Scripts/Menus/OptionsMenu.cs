a6eb419ca09da000aeb3bbcfc7c96b92c81542b2
stephdraggo <61036246+stephdraggo@users.noreply.github.com>
2020-11-02 14:34:01 +1100

    Menus switch between each other
    moving between menus and interfaces is functional

---
+++ Assets/Scripts/Menus/OptionsMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefence.Menus
+{
+    [AddComponentMenu("Menus/Options Menu")]
+    public class OptionsMenu : MonoBehaviour
+    {
+        #region Variables
+
+        #endregion
+        #region Properties
+
+        #endregion
+        #region Start
+        void Start()
+        {
+
+        }
+        #endregion
+        #region Update
+        void Update()
+        {
+
+        }
+        #endregion
+        #region Functions
+
+        #endregion
+    }
+}
