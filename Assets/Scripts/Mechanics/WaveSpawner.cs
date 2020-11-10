43672bf56f5ade1e713108d72afb70c5caceb480
stephdraggo <stephdraggo@users.noreply.github.com>
2020-11-10 15:50:37 +1100

    ehhhh
    spawns 1 lot of guys every time you click the button

--- Assets/Scripts/Mechanics/WaveSpawner.cs
+++ Assets/Scripts/Mechanics/WaveSpawner.cs
@@ -15,9 +15,10 @@ namespace TowerDefence.Mechanics.Spawning
         [SerializeField, Tooltip("0: pleb, 1: fast, 2: tank")]
         private GameObject[] enemyPrefabs;
 
-        private bool waveReady = false;
+        private bool waveReady = false,inWave=false;
 
-        private float currentTime = 0;
+
+        private float spawnRateTimer = 0,waveTimer=0;
         private EnemyManager enemyManager;
         #endregion
         #region Properties
@@ -36,7 +37,13 @@ namespace TowerDefence.Mechanics.Spawning
             enemyManager = EnemyManager.instance;
 
             waveNumber = 0;
-            totalWaves = 10; //for testing
+
+            //for testing
+            totalWaves = 10;
+            lengthOfWave = 5;
+            waveReady = false;
+
+            GameManager.gameDifficulty = GameDifficulty.Easy;
         }
         #endregion
         #region Update
@@ -54,41 +61,126 @@ namespace TowerDefence.Mechanics.Spawning
                 waveNumber++;
             }
 
+            if (inWave)
+            {
+                SpawnWave();
+                if (waveTimer > lengthOfWave)
+                {
+                    inWave = false;
+                }
+            }
 
 
-            //start wave (button?)
-            //spread wave spawning according to spawnrate
-            //decrease count of spawnables as they spawn
-
+            waveTimer += Time.deltaTime;
+            spawnRateTimer += Time.deltaTime;
         }
         #endregion
         #region Functions
+        public void StartWave()
+        {
+            inWave = true;
+            waveTimer = 0;
+        }
+        #region spawn wave
+        /// <summary>
+        /// Spawn contents of wave spread out over wave time.
+        /// </summary>
         private void SpawnWave()
         {
+            if (spawnRateTimer > localSpawnRate)
+            {
+                spawnRateTimer = 0; //reset timer
 
+                //spawn here
+                if (plebInWave > 0)
+                {
+                    SpawnEnemies(0, 4);
+                }
+                else if (fastInWave > 0)
+                {
+                    SpawnEnemies(1, 1);
+                }
+                else if (tankInWave > 0)
+                {
+                    SpawnEnemies(2, 2);
+                }
+                else
+                {
+                    waveReady = false; //this prepared wave has been used up
+                }
+
+            }
         }
+        #endregion
+        #region spawn enemies
+        /// <summary>
+        /// Spawn a given number of enemies of a given type.
+        /// </summary>
+        /// <param name="_type">type of enemy to spawn</param>
+        /// <param name="_amount">amount of that enemy type to spawn</param>
+        private void SpawnEnemies(int _type, int _amount)
+        {
+            //spawn amount of type
+            for (int i = 0; i < _amount; i++)
+            {
+                enemyManager.SpawnEnemy(transform, enemyPrefabs[_type]);
+            }
+
+            //decrease the number to spawn
+            switch (_type)
+            {
+                case 0:
+                    plebInWave -= _amount;
+                    break;
+                case 1:
+                    fastInWave -= _amount;
+                    break;
+                case 2:
+                    tankInWave -= _amount;
+                    break;
+
+                default:
+                    Debug.LogError("Index out of bounds, fam.");
+                    break;
+            }
+        }
+        #endregion
+        #region wave contents
+        /// <summary>
+        /// Determine contents of wave according to wave number and difficulty setting.
+        /// </summary>
         private void WaveContents()
         {
             #region base numbers by difficulty
-            //base easy values
-            int pBase = 5;
-            int fBase = 2;
-            int tBase = 1;
-
-            if (GameManager.gameDifficulty == GameDifficulty.Medium)
+            int pBase = 0;
+            int fBase = 0;
+            int tBase = 0;
+            switch (GameManager.gameDifficulty)
             {
-                pBase = 7;
-                fBase = 4;
-                tBase = 2;
-            }
-            else if (GameManager.gameDifficulty == GameDifficulty.Hard)
-            {
-                pBase = 10;
-                fBase = 8;
-                tBase = 4;
+                case GameDifficulty.None:
+                    Debug.LogError("No game difficulty detected, fam.");
+                    break;
+                case GameDifficulty.Easy:
+                    pBase = 8;
+                    fBase = 2;
+                    tBase = 1;
+                    break;
+                case GameDifficulty.Medium:
+                    pBase = 16;
+                    fBase = 4;
+                    tBase = 2;
+                    break;
+                case GameDifficulty.Hard:
+                    pBase = 32;
+                    fBase = 8;
+                    tBase = 4;
+                    break;
+                default:
+                    Debug.LogError("No game difficulty detected, fam.");
+                    break;
             }
             #endregion
-           
+
             //add to spawnables count according to wave number
             plebInWave += pBase * (waveNumber + 1);
             if (waveNumber >= 1)
@@ -101,25 +193,9 @@ namespace TowerDefence.Mechanics.Spawning
             }
         }
         #endregion
+    
+        #endregion
 
-        private void SpawnEnemies()
-        {
-            // increment time by delta time if the current time is less than spawnrate
-            if (currentTime < spawnRate)
-            {
-                currentTime += Time.deltaTime;
-            }
-            else
-            {
-                currentTime = 0;
-
-                // Attempt to spawn enemy
-                if (enemyManager != null)
-                {
-                    enemyManager.SpawnEnemy(transform, enemyPrefabs[0]);
-                }
-            }
-        }
 
 
     }
